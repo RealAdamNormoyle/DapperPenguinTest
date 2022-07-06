c4cbe4418be5b39ab9f3b8091514a20256d61e83
NormoyleAdam <contact@adamnormoyle.co.uk>
2022-07-06 20:26:32 +0100

    Added Storage, Producer and Consumer components

---
+++ DapperPenguinTest/Assets/Scripts/Components/ResourceProducer.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+public struct ResourceProducer : IComponentData {
+
+    public float ProductionProgress;
+    public int ProductionRate;
+}
