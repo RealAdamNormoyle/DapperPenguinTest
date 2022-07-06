38231cd24715e3981d539470d6bb90c7b77397a5
NormoyleAdam <contact@adamnormoyle.co.uk>
2022-07-06 20:23:17 +0100

    Added WorldEntity and GridPosition components

---
+++ DapperPenguinTest/Assets/Scripts/Components/GridPosition.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Unity.Entities;
+using Unity.Mathematics;
+
+public struct GridPosition : IComponentData, IEquatable<GridPosition>
+{
+    public float2 Value;
+    public bool IsBlocked;
+
+    //We only want to compare the positions here
+    public bool Equals(GridPosition other) {
+        return Value.Equals(other.Value);
+    }
+}
---
+++ DapperPenguinTest/Assets/Scripts/Components/WorldEntity.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+public struct WorldEntity : IComponentData {
+    public bool BlocksGrid { get; set; }
+}
